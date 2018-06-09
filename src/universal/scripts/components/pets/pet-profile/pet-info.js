872947cc8ce6193dcebcbd2e6ff5fcbd090794f2
Hayk Aghabekyan <haykaghabekyan@gmail.com>
2018-06-09 20:00:27 +0400

    Add pet facts (ui only)

--- src/universal/scripts/components/pets/pet-profile/pet-info.js
+++ src/universal/scripts/components/pets/pet-profile/pet-info.js
@@ -4,25 +4,38 @@ const PetInfo = ({ pet }) => {
     return (
         <div className="pet-profile">
             <div className="pet-profile-content">
-                <div className="pet-info">
-                    <div className="pet-info-picture">
-                        <img className="pet-info-img" src="/media/images/fake-dog/5.jpeg" />
+                <div className="pet-facts">
+                    <div className="pet-facts-picture">
+                        <img className="pet-facts-img" src="/media/images/fake-dog/5.jpeg" />
                     </div>
-                    <div className="pet-info-content">
-                        <h2 className="pet-info-name">Bobik</h2>
+                    <div className="pet-facts-content">
+                        <h2 className="pet-facts-name">Bobik</h2>
 
-                        <dl className="pet-info-list">
-                            <dt className="pet-info-label">Type</dt><dd className="pet-info-value">Dominic</dd>
-                            <dt className="pet-info-label">Breed</dt><dd className="pet-info-value">24</dd>
-                            <dt className="pet-info-label">Gender</dt><dd className="pet-info-value">foo@bar.com</dd>
+                        <dl className="pet-facts-list">
+                            <dt className="pet-facts-label">Type</dt><dd className="pet-facts-value">Dominic</dd>
+                            <dt className="pet-facts-label">Breed</dt><dd className="pet-facts-value">24</dd>
+                            <dt className="pet-facts-label">Gender</dt><dd className="pet-facts-value">foo@bar.com</dd>
                         </dl>
                     </div>
                 </div>
             </div>
             <div className="pet-info">
-                <div>
-                    Color
-                </div>
+                <dl className="pet-info-list">
+                    <dt className="pet-info-label">Pet ID</dt>
+                    <dd className="pet-info-value">56134561</dd>
+                </dl>
+                <dl className="pet-info-list">
+                    <dt className="pet-info-label">Color</dt>
+                    <dd className="pet-info-value">Light brown</dd>
+                </dl>
+                <dl className="pet-info-list">
+                    <dt className="pet-info-label">Age</dt>
+                    <dd className="pet-info-value">Young</dd>
+                </dl>
+                <dl className="pet-info-list">
+                    <dt className="pet-info-label">Size</dt>
+                    <dd className="pet-info-value">Medium (26-60 lbs, 25 kg)</dd>
+                </dl>
             </div>
         </div>
     );
