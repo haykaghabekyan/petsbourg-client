9f820633766dc7d54126cc166def18652a93317c
Hayk Aghabekyan <haykaghabekyan@gmail.com>
2018-11-29 20:13:27 +0400

    Add env variable

--- src/universal/scripts/configs/axios.js
+++ src/universal/scripts/configs/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import UniversalCookie from "universal-cookie";
 
 export const setAxiosConfigs = () => {
-    axios.defaults.baseURL = 'http://localhost:3003';
+    axios.defaults.baseURL = process && process.ENV.API_ENDPOINT ? process.ENV.API_ENDPOINT : "http://localhost:3003";
 
     const universalCookie = new UniversalCookie();
 
