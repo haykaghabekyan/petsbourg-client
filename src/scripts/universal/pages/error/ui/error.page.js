110d2f7f10284f0233c891c5b099d06d967e6c02
Hayk Aghabekyan <haykaghabekyan@gmail.com>
2019-01-21 02:19:54 +0400

    Update error page

--- src/scripts/universal/pages/error/ui/error.page.js
+++ src/scripts/universal/pages/error/ui/error.page.js
@@ -3,14 +3,14 @@ import { connect } from 'react-redux';
 import { renderRoutes } from 'react-router-config'
 import { MainLayout } from '../../../components/layouts/main';
 
-const ErrorPageContainer = ({ me, route }) => {
+const ErrorPageContainer = ({ auth, route }) => {
     return (
-        <MainLayout me={ me }>
+        <MainLayout user={ auth.user }>
             { renderRoutes(route.routes) }
         </MainLayout>
     );
 };
 
-const mapStateToProps = state => ({ me: state.me });
+const mapStateToProps = state => ({ auth: state.auth });
 
 export const ErrorPage = connect(mapStateToProps)(ErrorPageContainer);
