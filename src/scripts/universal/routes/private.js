0cba6015d7687192216955daeba7df413a3d0107
Hayk Aghabekyan <haykaghabekyan@gmail.com>
2019-01-31 01:55:17 +0400

    Remove server rendering for home page

--- src/scripts/universal/routes/private.js
+++ src/scripts/universal/routes/private.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { Redirect } from 'react-router-dom';
-import { observableFromStore } from '../utils/observable-from-store';
-import { filter, map, take } from 'rxjs/operators';
-import { HomePage, homePageLoadAction, HomeComponent } from '../pages/home';
+import { HomePage } from '../pages/home';
 import { PetEditPage } from '../pages/pet-edit';
 import { UserEditPage } from '../pages/user-edit';
 import { PetAddPage } from '../pages/pet-add';
@@ -13,21 +11,6 @@ export const privateRoutes = [
         path: '/',
         component: HomePage,
         exact: true,
-        routes: [{
-            path: '/',
-            component: HomeComponent,
-        }],
-        loadPage: (store) => {
-            store.dispatch(homePageLoadAction());
-
-            return {
-                ready: observableFromStore(store).pipe(
-                    map(state => !state.homePage.isLoading),
-                    filter(x => x === true),
-                    take(1)
-                ),
-            };
-        }
     },
     {
         path: '/pets/:petId/edit',
