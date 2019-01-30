58b762bbe642afde21ba99f869741ed57d491f04
Hayk Aghabekyan <haykaghabekyan@gmail.com>
2019-01-30 22:38:57 +0400

    Remove server side rendering for edit pet page

--- src/scripts/universal/routes/private.js
+++ src/scripts/universal/routes/private.js
@@ -3,7 +3,7 @@ import { Redirect } from 'react-router-dom';
 import { observableFromStore } from '../utils/observable-from-store';
 import { filter, map, take } from 'rxjs/operators';
 import { HomePage, homePageLoadAction, HomeComponent } from '../pages/home';
-import { PetEditPage, petEditPageLoadAction } from '../pages/pet-edit';
+import { PetEditPage } from '../pages/pet-edit';
 import { PetAddPage } from '../pages/pet-add';
 import { SearchPage } from '../pages/search';
 
@@ -32,17 +32,6 @@ export const privateRoutes = [
         path: '/pets/:petId/edit',
         component: PetEditPage,
         exact: true,
-        loadPage: (store, params) => {
-            store.dispatch(petEditPageLoadAction(params));
-
-            return {
-                ready: observableFromStore(store).pipe(
-                    map(state => !state.petEditPage.isLoading),
-                    filter(x => x === true),
-                    take(1)
-                ),
-            };
-        }
     },
     // {
     //     path: '/users/:id/edit',
@@ -62,19 +51,6 @@ export const privateRoutes = [
         component: PetAddPage,
         exact: true,
     },
-    // {
-    //     path: '/pets/:petId/edit',
-    //     component: MainLayout,
-    //     exact: true,
-    //     routes: [{
-    //         path: '/pets/:petId/edit',
-    //         component: PetProfileContainer,
-    //         routes: [{
-    //             path: '/pets/:petId/edit',
-    //             component: EditPetContainer,
-    //         }],
-    //     }],
-    // },
     {
         path: '/search',
         component: SearchPage,
