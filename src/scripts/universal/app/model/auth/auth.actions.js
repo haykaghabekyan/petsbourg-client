3413231bd528c3dd12b0efa4b2d3d694a407ac55
Hayk Aghabekyan <haykaghabekyan@gmail.com>
2019-02-05 02:14:18 +0400

    Add auth epic (to be implemented)

--- src/scripts/universal/app/model/auth/auth.actions.js
+++ src/scripts/universal/app/model/auth/auth.actions.js
@@ -2,6 +2,6 @@ export const SET_AUTH_ACTION = 'SET_AUTH_ACTION';
 export const setAuthAction = payload => {
     return {
         type: SET_AUTH_ACTION,
-        payload: payload,
+        payload,
     };
 };
