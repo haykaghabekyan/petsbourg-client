63833dde14f3ba3b95d9446ca5740ff50560fd53
Hayk Aghabekyan <haykaghabekyan@gmail.com>
2019-02-05 14:03:37 +0400

    Update input

--- src/scripts/universal/components/form-elements/input.js
+++ src/scripts/universal/components/form-elements/input.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-export const Input = ({ input, meta: { touched, error }, placeholder = '', type = 'text', border = true, className = '' }) => {
+export const Input = ({ input, meta: { touched, error }, placeholder = '', type = 'text', border = true, className = '', autoComplete = 'on' }) => {
     return (
         <div className={ `input-container ${ className } ${ (touched && error) ? 'input-container-error' : '' }` }>
-            <input className={`input-item ${ border ? '' : 'no-border' }`} autoComplete="off" { ...input } placeholder={ placeholder } type={ type } />
+            <input className={`input-item ${ border ? '' : 'no-border' }`} autoComplete={ autoComplete } { ...input } placeholder={ placeholder } type={ type } />
             { touched && error && <p className="input-error">{ error }</p> }
         </div>
     );
