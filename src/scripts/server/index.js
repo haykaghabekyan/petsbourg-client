d7164f9caabc21afeb4a25e497017d7dce1cb604
Hayk Aghabekyan <haykaghabekyan@gmail.com>
2019-02-13 15:35:03 +0400

    Fix

--- src/scripts/server/index.js
+++ src/scripts/server/index.js
@@ -81,7 +81,7 @@ app.get('*', authMiddleware, (req, res) => {
                           window.dataLayer = window.dataLayer || [];
                           function gtag(){dataLayer.push(arguments);}
                           gtag('js', new Date());
-                          gtag('config', ${ configs().tracking.google.trackingId });
+                          gtag('config', '${ configs().tracking.google.trackingId }');
                         </script>
                         <meta http-equiv="content-type" content="text/html" charset="utf-8" />
                         <meta name="viewport" content="width=device-width,initial-scale=1" />
