5d18efe1a0c20b32c3991603178f41f27c05ea80
Hayk Aghabekyan <haykaghabekyan@gmail.com>
2019-07-22 08:37:21 +0400

    Add contact us page

--- src/scripts/universal/pages/contact-us/ui/contact-us.page.js
+++ src/scripts/universal/pages/contact-us/ui/contact-us.page.js
@@ -1,14 +1,19 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { MainLayout } from '../../../components/layouts/main';
 
-export const ContactUsPage = () => {
-    const { auth } = this.props;
-
+export const ContactUs = ({ auth }) => {
     return (
         <MainLayout user={ auth.user } pets={ auth.pets }>
             <div>
-                Contact us
+                For cooperation requests you can contact us by <a href="mailto:haykaghabekyan@gmail.com">haykaghabekyan@gmail.com</a> email address.
             </div>
         </MainLayout>
     );
 };
+
+const mapStateToProps = state => ({
+    auth: state.auth,
+});
+
+export const ContactUsPage = connect(mapStateToProps)(ContactUs);
