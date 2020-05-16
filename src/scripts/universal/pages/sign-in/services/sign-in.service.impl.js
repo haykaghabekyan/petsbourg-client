272f04832ee9b52a824b4c1e57581518fcf7d7b5
Hayk Aghabekyan <haykaghabekyan@gmail.com>
2020-05-16 21:40:05 +0400

    added console.error

--- src/scripts/universal/pages/sign-in/services/sign-in.service.impl.js
+++ src/scripts/universal/pages/sign-in/services/sign-in.service.impl.js
@@ -1,31 +1,33 @@
 import axios from 'axios';
-import { UserServiceImpl } from '../../user/services/user.service.impl';
+import {UserServiceImpl} from '../../user/services/user.service.impl';
 
 export class SignInServiceImpl {
-    static signIn(req, res) {
-        const { email, password } = req.body;
+  static signIn(req, res) {
+    const {email, password} = req.body;
 
-        axios.post(`${ process.env.BACKEND_URL }/api/auth/sign-in`, { email, password })
-            .then(async ({ data }) => {
-                const { token, user } = data;
+    axios.post(`${process.env.BACKEND_URL}/api/auth/sign-in`, {email, password})
+      .then(async ({data}) => {
+        const {token, user} = data;
 
-                res.cookie('jwt', `Bearer: ${ token }`, { maxAge: 900000 });
+        res.cookie('jwt', `Bearer: ${token}`, {maxAge: 900000});
 
-                let pets = null;
-                try {
-                    pets = await UserServiceImpl.getUserPets(user._id);
-                } catch(error) {}
+        let pets = null;
+        try {
+          pets = await UserServiceImpl.getUserPets(user._id);
+        } catch (error) {
+        }
 
-                res.status(200).send({
-                    success: true,
-                    user: user,
-                    pets: pets,
-                });
-            })
-            .catch(error => {
-                res.status(400).send({
-                    ...error.response.data
-                });
-            });
-    }
+        res.status(200).send({
+          success: true,
+          user: user,
+          pets: pets,
+        });
+      })
+      .catch(error => {
+        console.error(error);
+        res.status(400).send({
+          ...error.response.data
+        });
+      });
+  }
 }
