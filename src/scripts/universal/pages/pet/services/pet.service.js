f819579c05dd35750a24bb1068c8ce9b36b01d00
Hayk Aghabekyan <haykaghabekyan@gmail.com>
2020-05-16 20:52:30 +0400

    Reverted changes

--- src/scripts/universal/pages/pet/services/pet.service.js
+++ src/scripts/universal/pages/pet/services/pet.service.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export class PetService {
   static loadPetPage(petId) {
-    return axios.get(`/api/pet-page/${petId}`)
+    return axios.get(`${process.env.FRONTEND_URL}/api/pet-page/${petId}`)
       .then(({data}) => data);
   }
 }
