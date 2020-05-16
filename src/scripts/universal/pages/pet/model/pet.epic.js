5489fadd98059739392ad266f09b89d4b5ebd421
Hayk Aghabekyan <haykaghabekyan@gmail.com>
2020-05-17 00:27:46 +0400

    Added console.error

--- src/scripts/universal/pages/pet/model/pet.epic.js
+++ src/scripts/universal/pages/pet/model/pet.epic.js
@@ -1,28 +1,28 @@
-import { from, of } from 'rxjs';
-import { filter, mergeMap, map, catchError } from 'rxjs/operators';
-import { PetService } from '../services/pet.service';
-import { PET_PAGE_LOAD_ACTION_TYPE, petPageLoadSucceededAction, petPageLoadFailedAction } from './pet.actions';
+import {from, of} from 'rxjs';
+import {filter, mergeMap, map, catchError} from 'rxjs/operators';
+import {PetService} from '../services/pet.service';
+import {PET_PAGE_LOAD_ACTION_TYPE, petPageLoadSucceededAction, petPageLoadFailedAction} from './pet.actions';
 
 export const petPageLoadEpic = action$ => {
-    return action$.pipe(
-        filter(action => action.type === PET_PAGE_LOAD_ACTION_TYPE),
-        mergeMap((action) => {
-            const { petId } = action.payload;
-            const promise = PetService.loadPetPage(petId);
+  return action$.pipe(
+    filter(action => action.type === PET_PAGE_LOAD_ACTION_TYPE),
+    mergeMap((action) => {
+      const {petId} = action.payload;
+      const promise = PetService.loadPetPage(petId);
 
-            return from(promise)
-                .pipe(
-                    map(result => {
-                        const { pet, user, pets } = result;
+      return from(promise)
+        .pipe(
+          map(result => {
+            const {pet, user, pets} = result;
 
-                        return petPageLoadSucceededAction({ pet, user, pets });
-                    }),
-                    catchError(({ response }) => {
-                        const { errors } = response.data;
-
-                        return of(petPageLoadFailedAction(errors.message));
-                    })
-                );
-        })
-    );
+            return petPageLoadSucceededAction({pet, user, pets});
+          }),
+          catchError(({response}) => {
+            console.error(response);
+            const {errors} = response.data;
+            return of(petPageLoadFailedAction(errors.message));
+          })
+        );
+    })
+  );
 };
